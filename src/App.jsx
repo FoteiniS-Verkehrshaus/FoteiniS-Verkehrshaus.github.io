85ffe556d3d7903c7494cd608f8dcdf18605ed03
Salveridou Foteini <foteini.salveridou@verkehrshaus.ch>
2023-08-29 11:36:49 +0200

    sandbox

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/KUsC7MBNueW1G-7V/scene.splinecode" />
+  );
+}
